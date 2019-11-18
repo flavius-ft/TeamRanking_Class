b347c1735b2525c42574636c4094e1236dd1a32f
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2019-11-18 13:42:47 +0200

    first commit

---
+++ ClaseBubleSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClaseBubleSort
+{
+    class SoccerTeams
+    {
+        private string Name;
+        private int Points;
+
+        public SoccerTeams(string name, int points)
+        {
+            this.Name = name;
+            this.Points = points;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
